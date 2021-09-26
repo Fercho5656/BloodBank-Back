191a7d2946f9779c9dc482d58eb3f95821e51095
Fercho5656 <morenomoranfernando@gmail.com>
2021-09-25 21:01:29 -0500

    Added many to many relationship

--- Context/BloodBankContext.cs
+++ Context/BloodBankContext.cs
@@ -5,12 +5,24 @@ namespace bloodbank.Context {
     public class BloodBankContext : DbContext {
         public BloodBankContext() { }
         public BloodBankContext(DbContextOptions<BloodBankContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<BloodGroup_BloodBank>()
+            .HasOne(b => b.BloodBank)
+            .WithMany(b => b.BloodGroups_BloodBanks)
+            .HasForeignKey(b => b.BloodBankId);
+
+            modelBuilder.Entity<BloodGroup_BloodBank>()
+            .HasOne(b => b.BloodGroup)
+            .WithMany(b => b.BloodGroups_BloodBanks)
+            .HasForeignKey(b => b.BloodGroupId);
+        }
         public DbSet<ContactInfo> ContactInfo { get; set; }
         public DbSet<Patient> Patients { get; set; }
         public DbSet<Disease> Diseases { get; set; }
         public DbSet<FamilyGroup> FamilyGroups { get; set; }
         public DbSet<BloodGroup> BloodGroups { get; set; }
         public DbSet<BloodBank> BloodBanks { get; set; }
+        public DbSet<BloodGroup_BloodBank> BloodGroups_BloodBanks { get; set; }
         public DbSet<IncomingBlood> IncomingBlood { get; set; }
         public DbSet<OutgoingBlood> OutgoingBlood { get; set; }
         public DbSet<Hospital> Hospitals { get; set; }
@@ -19,11 +31,6 @@ namespace bloodbank.Context {
         public DbSet<Request> Requests { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
-        public DbSet<BloodGroup_BloodBank> BloodGroups_BloodBanks { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder) {
-            modelBuilder.Entity<BloodGroup_BloodBank>()
-            .HasKey(o => new { o.BloodGroupId, o.BloodBankId });
-        }
     }
 }
