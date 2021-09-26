f73fc7a5ab7384f3389536f71a3056f61b6de770
Fercho5656 <morenomoranfernando@gmail.com>
2021-09-25 21:05:05 -0500

    Removed DataAnnotations

--- Models/BloodGroup_BloodBank.cs
+++ Models/BloodGroup_BloodBank.cs
@@ -3,14 +3,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace bloodbank.Models {
     public class BloodGroup_BloodBank {
-        [Key, Column(Order = 0)]
+        public int Id { get; set; }
         public int BloodGroupId { get; set; }
-        [Key, Column(Order = 1)]
-        public int BloodBankId { get; set; }
-        [ForeignKey("BloodGroupId")]
         public BloodGroup BloodGroup { get; set; }
-        [ForeignKey("BloodBankId")]
+
+        public int BloodBankId { get; set; }
         public BloodBank BloodBank { get; set; }
+
         [Required]
         public int Quantity { get; set; }
     }
