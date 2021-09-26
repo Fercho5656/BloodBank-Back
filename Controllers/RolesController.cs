9bc1d5da857d0d24dbe0e49d5ca65744b31d3c29
Fercho5656 <fercho5_5_5_5@hotmail.com>
2021-09-26 01:00:22 -0500

    Finished Controller to handle Roles Requests

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -3,72 +3,48 @@ using bloodbank.Context;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using bloodbank.Models;
+using BloodBank_Backend.Services;
+using BloodBank_Backend.ViewModels;
 
 namespace bloodbank.Controllers {
     [ApiController]
     [Route("api/[controller]")]
     public class RolesController : ControllerBase {
 
-        private readonly BloodBankContext Context;
+        private readonly RolesService _rolesService;
 
-        public RolesController(BloodBankContext context) {
-            Context = context;
+        public RolesController(RolesService rolesService) {
+            _rolesService = rolesService;
         }
-        //GET
+
         [HttpGet]
-        public async Task<ActionResult> GetAll() {
-            var roles = await Context.Roles.ToListAsync();
-            if (roles == null) {
-                return NotFound();
-            }
+        public IActionResult GetAll() {
+            var roles = _rolesService.GetAll();
             return Ok(roles);
         }
-        //GET/5
+
         [HttpGet("{id}")]
-        public async Task<ActionResult> GetRole(int? id) {
-            if (id == null) {
-                return BadRequest();
-            }
-            Role role = await Context.Roles.FindAsync(id);
-            if (role == null) {
-                return NotFound();
-            }
+        public IActionResult GetRole(int id) {
+            var role = _rolesService.GetRole(id);
             return Ok(role);
         }
-        //POST
+
         [HttpPost]
-        public async Task<ActionResult> Create(Role role) {
-            if (ModelState.IsValid) {
-                await Context.Roles.AddAsync(role);
-                await Context.SaveChangesAsync();
-            }
-            return CreatedAtAction(nameof(Create), new { id = role.Id }, role);
+        public IActionResult Create([FromBody] RoleVM role) {
+            _rolesService.AddRole(role);
+            return Ok();
         }
 
-        //DELETE/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult> Delete(int? id) {
-            if (id == null) {
-                return BadRequest();
-            }
-            Role role = await Context.Roles.FindAsync(id);
-            if (role is null) {
-                return NotFound();
-            }
-            Context.Roles.Remove(role);
-            await Context.SaveChangesAsync();
-            return NoContent();
-        }
-        //PUT/5
         [HttpPut("{id}")]
-        public async Task<ActionResult> Update(int? id, Role role) {
-            if (id == null || id != role.Id) {
-                return BadRequest();
-            }
-            Context.Entry(role).State = EntityState.Modified;
-            await Context.SaveChangesAsync();
+        public IActionResult Update(int id, [FromBody] RoleVM role) {
+            var updatedRole = _rolesService.Update(id, role);
+            return Ok(updatedRole);
+        }
 
-            return NoContent();
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) {
+            _rolesService.Delete(id);
+            return Ok();
         }
     }
 }
