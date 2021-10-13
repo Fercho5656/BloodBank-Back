90f53a230f0d9653057a0fe861e46c105e51d201
Fercho5656 <fercho5_5_5_5@hotmail.com>
2021-10-13 02:03:02 -0500

    Service with own methods to handle relationships

---
+++ Services/BloodBankService.cs
@@ -0,0 +1,35 @@
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using bloodbank.Context;
+using bloodbank.Models;
+using BloodBank_Backend.Base;
+using BloodBank_Backend.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloodBank_Backend.Services {
+    public class BloodBankService : EntityBaseRepository<BloodBank>, IBloodBankService {
+
+        private readonly BloodBankContext _context;
+        private readonly IContactInfoService _contactInfoService;
+        private readonly IMapper _mapper;
+        public BloodBankService(BloodBankContext context, IContactInfoService contactInfoService, IMapper mapper) : base(context) {
+            _context = context;
+            _contactInfoService = contactInfoService;
+            _mapper = mapper;
+        }
+
+        public async Task AddBloodBank(BloodBank bloodBank) {
+            await Add(bloodBank);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<BloodBank> GetBloodBank(int id) {
+            var bloodBank = await _context.BloodBanks
+                .Include(c => c.ContactInfo)
+                .FirstOrDefaultAsync(b => b.Id == id);
+
+            return bloodBank;
+        }
+    }
+}
