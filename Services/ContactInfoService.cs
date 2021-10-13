33e9ff914025bd2a12459b7f8edd50e66694a762
Fercho5656 <fercho5_5_5_5@hotmail.com>
2021-10-13 02:01:46 -0500

    Implementing service interface

--- Services/ContactInfoService.cs
+++ Services/ContactInfoService.cs
@@ -1,59 +1,14 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using bloodbank.Context;
 using bloodbank.Models;
+using BloodBank_Backend.Base;
 using BloodBank_Backend.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace BloodBank_Backend.Services {
-    public class ContactInfoService {
-        private BloodBankContext _context;
-        public ContactInfoService(BloodBankContext context) {
-            _context = context;
-        }
-
-        public void AddContactInfo(ContactInfoVM contactInfo) {
-            var _contactInfo = new ContactInfo() {
-                Address = contactInfo.Address,
-                PostalCode = contactInfo.PostalCode,
-                City = contactInfo.City,
-                State = contactInfo.PostalCode,
-                Country = contactInfo.Country,
-                Email = contactInfo.Email,
-                Phone = contactInfo.Phone
-            };
-            _context.ContactInfo.Add(_contactInfo);
-            _context.SaveChanges();
-        }
-
-        public List<ContactInfo> GetAll() {
-            return _context.ContactInfo.ToList();
-        }
-
-        public ContactInfo GetContactInfo(int id) {
-            return _context.ContactInfo.FirstOrDefault(contact => contact.Id == id);
-        }
-
-        public ContactInfo Update(int id, ContactInfoVM contactInfo) {
-            var _contactInfo = _context.ContactInfo.FirstOrDefault(contact => contact.Id == id);
-            if (_contactInfo != null) {
-                _contactInfo.Address = contactInfo.Address;
-                _contactInfo.PostalCode = contactInfo.PostalCode;
-                _contactInfo.City = contactInfo.City;
-                _contactInfo.State = contactInfo.State;
-                _contactInfo.Country = contactInfo.Country;
-                _contactInfo.Email = contactInfo.Email;
-                _contactInfo.Phone = contactInfo.Phone;
-                _context.SaveChanges();
-            }
-            return _contactInfo;
-        }
-
-        public void Delete(int id) {
-            var _contactInfo = _context.ContactInfo.FirstOrDefault(contact => contact.Id == id);
-            if (_contactInfo != null) {
-                _context.ContactInfo.Remove(_contactInfo);
-                _context.SaveChanges();
-            }
-        }
+    public class ContactInfoService : EntityBaseRepository<ContactInfo>, IContactInfoService {
+        public ContactInfoService(BloodBankContext context) : base(context) { }
     }
 }
--- Services/RolesService.cs
+++ Services/RolesService.cs
@@ -1,47 +1,15 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using bloodbank.Context;
 using bloodbank.Models;
+using BloodBank_Backend.Base;
 using BloodBank_Backend.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace BloodBank_Backend.Services {
-    public class RolesService {
-        private BloodBankContext _context;
-        public RolesService(BloodBankContext context) {
-            _context = context;
-        }
+    public class RolesService : EntityBaseRepository<Role>, IRolesServices {
+        public RolesService(BloodBankContext context) : base(context){}
 
-        public void AddRole(RoleVM role) {
-            var _role = new Role() {
-                RoleName = role.RoleName
-            };
-            _context.Roles.Add(_role);
-            _context.SaveChanges();
-        }
-
-        public List<Role> GetAll() {
-            return _context.Roles.ToList();
-        }
-
-        public Role GetRole(int id) {
-            return _context.Roles.FirstOrDefault(role => role.Id == id);
-        }
-
-        public Role Update(int id, RoleVM role) {
-            var _role = _context.Roles.FirstOrDefault(role => role.Id == id);
-            if (_role != null) {
-                _role.RoleName = role.RoleName;
-                _context.SaveChanges();
-            }
-            return _role;
-        }
-
-        public void Delete(int id) {
-            var _role = _context.Roles.FirstOrDefault(role => role.Id == id);
-            if (_role != null) {
-                _context.Roles.Remove(_role);
-                _context.SaveChanges();
-            }
-        }
     }
 }
