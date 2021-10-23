9a964a0d6c0bc475d01609b9a7f24d5f3bad3c0c
Fercho5656 <morenomoranfernando@gmail.com>
2021-10-23 14:17:42 -0500

    SaveHospital model

---
+++ ViewModels/SaveHospitalVM.cs
@@ -0,0 +1,10 @@
+namespace bloodbank.ViewModels {
+    public class SaveHospitalVM {
+        public string Name { get; set; }
+
+        //Relationships
+        //ContactInfo
+        public int ContactInfoId { get; set; }
+
+    }
+}
