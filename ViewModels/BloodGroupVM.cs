251d8af4ea6d695415e194aa948506149e763fdc
Fercho5656 <fercho5_5_5_5@hotmail.com>
2021-10-27 14:39:27 -0500

    Removed quantity from bloodGroup

--- ViewModels/BloodGroupVM.cs
+++ ViewModels/BloodGroupVM.cs
@@ -3,7 +3,7 @@ namespace bloodbank.ViewModels {
         public int Id { get; set; }
         public string BloodType { get; set; }
         public char RH { get; set; }
-        public int Quantity { get; set; }
+        //public int Quantity { get; set; }
 
     }
 }
