a52a042191442262c7f35b68d3c946bad0e3d0ad
Fercho5656 <morenomoranfernando@gmail.com>
2021-10-26 21:04:07 -0500

    Removed required tag on quantity Added Id PK -maybe not necessary-

--- Models/BloodGroup_BloodBank.cs
+++ Models/BloodGroup_BloodBank.cs
@@ -1,16 +1,15 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using BloodBank_Backend.Base;
 
 namespace bloodbank.Models {
-    public class BloodGroup_BloodBank {
-        //public int Id { get; set; }
+    public class BloodGroup_BloodBank : IEntityBase {
+        public int Id { get; set; }
         public int BloodGroupId { get; set; }
         public BloodGroup BloodGroup { get; set; }
 
         public int BloodBankId { get; set; }
         public BloodBank BloodBank { get; set; }
-
-        [Required]
         public int Quantity { get; set; }
     }
 }
