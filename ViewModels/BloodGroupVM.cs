8dbb5f5de985be0d1cfd276889924425cf22c088
Fercho5656 <morenomoranfernando@gmail.com>
2021-10-26 20:58:21 -0500

    BloodGroup save and read models

---
+++ ViewModels/BloodGroupVM.cs
@@ -0,0 +1,9 @@
+namespace bloodbank.ViewModels {
+    public class BloodGroupVM {
+        public int Id { get; set; }
+        public string BloodType { get; set; }
+        public char RH { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
---
+++ ViewModels/SaveBloodGroupVM.cs
@@ -0,0 +1,7 @@
+namespace bloodbank.ViewModels {
+    public class SaveBloodGroupVM {
+        public string BloodType { get; set; }
+        public char RH { get; set; }
+        // public int Quantity { get; set; }
+    }
+}
