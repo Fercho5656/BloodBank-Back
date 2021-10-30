03f19e74eed506d4877ab80e7620ebf591beee09
Fercho5656 <morenomoranfernando@gmail.com>
2021-10-30 12:24:41 -0500

    Donor endpoint controller

---
+++ Controllers/DonorController.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using bloodbank.Models;
+using bloodbank.Services;
+using BloodBank_Backend.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bloodbank.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DonorController : ControllerBase {
+        private readonly IDonorService _service;
+        private readonly IMapper _mapper;
+        public DonorController(IDonorService service, IMapper mapper) {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll() {
+            var result = await _service.GetAll(b => b.BloodGroup, c => c.ContactInfo, b => b.BloodBank);
+            var DonorsVM = _mapper.Map<IEnumerable<DonorVM>>(result);
+            return Ok(DonorsVM);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id) {
+            var result = await _service.Get(id);
+            if (result == null) return NotFound();
+            var DonorVM = _mapper.Map<DonorVM>(result);
+            return Ok(DonorVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] SaveDonorVM saveNewDonor) {
+            var newDonor = _mapper.Map<Donor>(saveNewDonor);
+            await _service.Add(newDonor);
+            var newDonorVM = _mapper.Map<DonorVM>(newDonor);
+            return CreatedAtAction(nameof(Create), new { id = newDonorVM.Id }, newDonorVM);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] SaveDonorVM saveDonor) {
+            var newDonor = _mapper.Map<SaveDonorVM, Donor>(saveDonor);
+            newDonor.Id = id;
+            var result = await _service.Update(id, newDonor);
+            if (result == null) return NotFound();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id) {
+            var donor = await _service.Get(id);
+            if (donor == null) return NotFound();
+            await _service.Delete(id);
+            return NoContent();
+        }
+    }
+}
